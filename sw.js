659a879354aef4e85c22b17bcb7c6f628aa14144
NosikovVG <Nosikovvg@mail.ru>
2026-09-25 19:30:23 +0300

    Update sw.js
    Поднял версию кеша

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Позволяет работать оффлайн после первого запуска.
    Для обновления версии приложения — поднять CACHE_VERSION. */
 
-const CACHE_VERSION = 'plenka-v3.0.0';
+const CACHE_VERSION = 'plenka-v3.0.1';
 const CACHE_SHELL = 'plenka-shell';
 const CACHE_MODELS = 'plenka-models';
 
